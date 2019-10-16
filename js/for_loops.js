dce4e950b53dfaeedf44a345eb408877b8200a57
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2019-10-16 10:10:24 -0500

    Started 3rd exercise for for-loops

--- js/for_loops.js
+++ js/for_loops.js
@@ -8,11 +8,19 @@
 
 // showMultiplicationTable(7);
 
+// for (var i = 1; i <= 10; i++) {
+//     var randomNumber = Math.floor(Math.random() * 180) + 20;
+//     if (randomNumber % 2 === 0) {
+//         console.log(randomNumber + " is even.");
+//     } else {
+//         console.log(randomNumber + " is odd.");
+//     }
+// }
+
 for (var i = 1; i <= 10; i++) {
-    var randomNumber = Math.floor(Math.random() * 180) + 20;
-    if (randomNumber % 2 === 0) {
-        console.log(randomNumber + " is even.");
-    } else {
-        console.log(randomNumber + " is odd.");
+    var counter = 0;
+    while (counter !== i) {
+        console.log(i);
+        counter += 1;
     }
 }
