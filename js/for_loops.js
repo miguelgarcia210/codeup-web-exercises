64f25144bbe72b8f849f45455024579a61f0e3d8
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2019-10-16 09:33:42 -0500

    Completed 1st exercise for for-loops

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,1 +1,9 @@
 "use strict";
+
+function showMultiplicationTable(x) {
+    for (var i = 1; i <= 10; i++) {
+        console.log(x + " x " + i + " = " + (x * i));
+    }
+}
+
+showMultiplicationTable(7);
