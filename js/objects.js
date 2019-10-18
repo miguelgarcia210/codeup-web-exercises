476a789cd97fae2e9abf4493d3976a5f69fa5962
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2019-10-18 13:46:07 -0500

    Completed 1st TODO

--- js/objects.js
+++ js/objects.js
@@ -12,6 +12,14 @@
      *  > console.log(person.lastName) // "Sanchez"
      */
 
+    var person = {
+        firstName: "Miguel",
+        lastName: "Garcia"
+    };
+
+    console.log(person.firstName);
+    console.log(person.lastName);
+
     /**
      * TODO:
      * Add a sayHello method to the person object that returns a greeting using
