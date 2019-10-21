8472004b0134811530a50257081252c1ce8db8f6
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2019-10-20 22:58:07 -0500

    Formatted files

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,36 +1,31 @@
 "use strict";
 
-// function showMultiplicationTable(x) {
-//     for (var i = 1; i <= 10; i++) {
-//         console.log(x + " x " + i + " = " + (x * i));
-//     }
-// }
+function showMultiplicationTable(x) {
+    for (var i = 1; i <= 10; i++) {
+        console.log(x + " x " + i + " = " + (x * i));
+    }
+}
 
-// showMultiplicationTable(7);
+showMultiplicationTable(7);
 
-// for (var i = 1; i <= 10; i++) {
-//     var randomNumber = Math.floor(Math.random() * 180) + 20;
-//     if (randomNumber % 2 === 0) {
-//         console.log(randomNumber + " is even.");
-//     } else {
-//         console.log(randomNumber + " is odd.");
-//     }
-// }
+for (var i = 1; i <= 10; i++) {
+    var randomNumber = Math.floor(Math.random() * 180) + 20;
+    if (randomNumber % 2 === 0) {
+        console.log(randomNumber + " is even.");
+    } else {
+        console.log(randomNumber + " is odd.");
+    }
+}
 
-// for (var i = 1; i <= 9; i++) {
-//     var string = i.toString();
-//     console.log(string.repeat(i));
-// }
+for (var i = 1; i <= 9; i++) {
+    var string = i.toString();
+    console.log(parseFloat(string.repeat(i)));
+}
 
-// Done in class
-// for (var i = 1; i < 10; i++) {
-//     var str = "";
-//     for (var y = 1; y <= i; y++) {
-//         str += i;
-//         console.log(str)
-//     }
-// }
-
-// for (var i = 100; i !== 0; i -= 5) {
-//     console.log(i);
-// }
+for (var i = 1; i < 10; i++) {
+    var str = "";
+    for (var y = 1; y <= i; y++) {
+        str += i;
+    }
+    console.log(str);
+}
--- js/while.js
+++ js/while.js
@@ -1,9 +1,9 @@
 "use strict";
 
-// var i = 1;
-// while (i < 65536) {
-//     console.log(i *= 2);
-// }
+var i = 1;
+while (i < 65536) {
+    console.log(i *= 2);
+}
 
 // An ice cream seller can't go home until she sells all of her cones.
 // Write a JS program that generates a random number between 50 and 100 representing the amount of cones to sell.
@@ -15,22 +15,22 @@
 
 // This is how you get a random number between 50 and 100
 var allCones = Math.floor(Math.random() * 50) + 50;
-// This expression will generate a random number between 1 and 5
+// This expression BELOW will generate a random number between 1 and 5
 // Math.floor(Math.random() * 5) + 1;
 
-// do {
-//     console.log(allCones + " cones available.");
-//     var number = Math.floor(Math.random() * 5) + 1;
-//
-//     if (number > allCones){
-//         console.log("Cannot sell you " + number + " cone(s). I only have " + allCones + ".");
-//         continue;
-//     }
-//
-//     console.log(number + " cone(s) sold");
-//     allCones -= number;
-//
-//     if (allCones === 0) {
-//         console.log("Yay! I sold them all!")
-//     }
-// } while(allCones !== 0);
+do {
+    console.log(allCones + " cones available.");
+    var number = Math.floor(Math.random() * 5) + 1;
+
+    if (number > allCones){
+        console.log("Cannot sell you " + number + " cone(s). I only have " + allCones + ".");
+        continue;
+    }
+
+    console.log(number + " cone(s) sold");
+    allCones -= number;
+
+    if (allCones === 0) {
+        console.log("Yay! I sold them all!")
+    }
+} while(allCones !== 0);
