27cbe71fe514992ee25d5d6a5afd212747352c8f
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2019-10-21 10:54:55 -0500

    Updated script

--- js/circle.js
+++ js/circle.js
@@ -9,9 +9,7 @@
             // TODO: complete this method
             // hint: area = pi * radius^2
 
-            var area = Math.PI * (Math.pow(circle.radius,2));
-
-            return area; // TODO: return the proper value
+            return Math.PI * (Math.pow(circle.radius, 2)); // TODO: return the proper value
         },
 
         logInfo: function (doRounding) {
@@ -19,13 +17,14 @@
 
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
-            if (doRounding === true) {
-                return Math.round(circle.getArea);
+            var area;
+            if (doRounding) {
+                area = Math.round(circle.getArea());
             } else {
-                return circle.getArea;
+                area = circle.getArea();
             }
 
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + area);
         }
     };
 
