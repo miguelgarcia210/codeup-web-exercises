6873eb5e6a4cb272b56e0156ce80604e96f23273
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2019-10-20 23:19:20 -0500

    Updated code

--- js/iterating.js
+++ js/iterating.js
@@ -22,26 +22,29 @@
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
+    console.log("---");
 
-    // console.log(names[0]);
-    // console.log(names[1]);
-    // console.log(names[2]);
-    // console.log(names[3]);
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
 
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
+    console.log("---");
 
-    // for (var i = 1; i < names.length; i++) {
-    //     console.log(names[i]);
-    // }
+    for (var i = 0; i < names.length; i++) {
+        console.log(names[i]);
+    }
 
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
+    console.log("---");
 
     names.forEach(function (name) {
         console.log(name);
@@ -60,8 +63,10 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+    console.log("---");
 
     var testArr = [1, 2, 3, 4, 5];
+    console.log(testArr);
 
     function firstElement(x) {
         return x[0];
