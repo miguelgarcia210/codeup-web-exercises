fa7706ee8cc802a2da50c89c100b84581877db26
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2019-10-24 09:50:11 -0500

    Completed morning warmup

--- js/morning-warmup.js
+++ js/morning-warmup.js
@@ -202,3 +202,17 @@ function createPersonObject(name, age) {
     }
     return person;
 }
+
+// Write FizzBuzz in JavaScript
+
+for (var i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0 ) {
+        console.log("Fizz");
+    } else if (i % 5 === 0 ) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
