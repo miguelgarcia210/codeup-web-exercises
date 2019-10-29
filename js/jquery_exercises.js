86f951689ebd958c40f4eb51dcb6c7475a2fb492
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2019-10-29 11:53:31 -0500

    Completed Class Selectors

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,21 +1,34 @@
 "use strict";
 
-$(document).ready(function() {
-    alert( 'The DOM has finished loading!' );
+$(document).ready(function () {
+    alert('The DOM has finished loading!');
 });
 
 // TODO: Id Selectors
+
 //  1.Create content in your HTML file using at least the following elements: h1, p, ul, li, div.
 //  2. Add several attributes to your elements; you will need both id and class attributes.
+
 //  3. Use jQuery to select an element by the id. Alert the contents of the element.
+var myDivContents = $("#myDiv").html();
+alert(myDivContents);
+
 //  4. Update the jQuery code to select and alert a different id.
+var myListContainer = $("#list-container").html();
+alert(myListContainer);
+
 //  5. Use the same id on 2 elements. How does this change the jQuery selection?
+// SELECTS THE FIRST ID ONLY
+// var duplicateListItemID = $("#list-item-6").html();
+// alert(duplicateListItemID);
+
 //  6. Remove the duplicate id. Each id should be unique on that page.
 
 // TODO: Class Selectors
 //  1. Remove your custom jQuery code from previous exercises.
 //  2. Update your code so that at least 3 different elements have the same class named codeup.
 //  3. Using jQuery, create a border around all elements with the class codeup that is 1 pixel wide and red.
+$(".codeup").css("border", "1px solid red");
 //  4. Remove the class from one of the elements. Refresh and test that the border has been removed.
 //  5. Give another element an id of codeup. Does this element get a border now?
 
