8e090d37948b0e762ded15fd6496e4af23ad8400
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2019-10-31 13:20:04 -0500

    Completed exercise

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -4,3 +4,11 @@ $(document).ready(function () {
     $("dd").addClass("invisible");
 });
 
+$("#toggle-invisibility").click(function(event) {
+    event.preventDefault();
+    $("dd").toggleClass("invisible");
+});
+
+$("dt").click(function (event) {
+   $(this).toggleClass("highlight");
+});
