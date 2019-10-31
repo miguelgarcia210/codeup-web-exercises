0144757045001a30fc5ffbd59c6c824875b655c7
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2019-10-31 15:23:15 -0500

    Implemented button to change last li style of each ul

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -4,11 +4,24 @@ $(document).ready(function () {
     $("dd").addClass("invisible");
 });
 
-$("#toggle-invisibility").click(function(event) {
+$("#toggle-invisibility").click(function (event) {
     event.preventDefault();
     $("dd").toggleClass("invisible");
 });
 
 $("dt").click(function (event) {
-   $(this).toggleClass("highlight");
+    $(this).toggleClass("highlight");
+});
+
+// -------------------------------------------------------
+$("#button").click(function (event) {
+    $("ul").each(function () {
+        $(this).children().last().toggleClass("yellow-back");
+    });
+    $(this).toggleClass("park-button");
+    if ($(this).hasClass("park-button")) {
+        $(this).html("Codeup");
+    } else {
+        $(this).html("Don't Click!")
+    }
 });
