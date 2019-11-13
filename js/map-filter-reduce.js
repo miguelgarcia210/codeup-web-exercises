1b5c710d562e798cb59dea40295e62838e6d32e1
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2019-11-13 11:15:39 -0600

    Completed MFR exercise

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -39,7 +39,7 @@ const users = [
 ];
 
 // TODO: Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
-const trilingualUsers = users.filter( user => user.languages.length >= 3);
+const trilingualUsers = users.filter(user => user.languages.length >= 3);
 console.log(trilingualUsers);
 
 // TODO: Use .map to create an array of strings where each element is a user's email address.
@@ -48,17 +48,26 @@ console.log(userEmails);
 // TODO: Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
 const totalExpYrs = users.reduce((total, user) => {
     return total + user.yearsOfExperience;
-},0);
+}, 0);
 const avgExpYrs = user => totalExpYrs / user.length;
 console.log(avgExpYrs(users));
 // TODO: Use .reduce to get the longest email from the list of users.
-const longestUserEmail = users.reduce((user1, user2) => {
-    console.log(user1.email.length);
-    console.log(user2.email.length);
-});
+const longestEmail = userEmails.reduce((longest, user) => {
+    if (user.length > longest.length) {
+        longest = user;
+    }
+    return longest
+}, userEmails[0]);
 
-longestUserEmail();
-// console.log(longestUserEmail);
+console.log(longestEmail);
 // TODO: Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
-// TODO:
-//  BONUS: Use .reduce to get the unique list of languages from the list of users.
+const userNames = users.map(user => user.name);
+const userNamesString = `${userNames}.`;
+console.log(userNamesString);
+//  TODO: BONUS: Use .reduce to get the unique list of languages from the list of users.
+const listOfLanguages = Array.from(users.reduce((accumulator,user) => {
+    user.languages.forEach(language => accumulator.add(language));
+    return accumulator;
+}, new Set()));
+
+console.log(listOfLanguages);
